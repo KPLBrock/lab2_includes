a8ed792ad12cd050842df2c16cab1362d5864e8f
Joshua S Kling <klin5995@mlc104.csumb.edu>
2015-09-10 12:20:03 -0700

    Adding letter W

--- ledBoard.inc.php
+++ ledBoard.inc.php
@@ -106,10 +106,10 @@ function drawLetter($letter, $color) {
 					break;
 					
 				case "F" :
-					if (1) {
+					if ($i < 2 || $j < 2 || $i == 4) {
 						$colorToDisplay = $color;
 						$letterToDisplay = $letter;
-					}
+					}	
 					break;
 					
 				case "G" :
@@ -120,7 +120,7 @@ function drawLetter($letter, $color) {
 					break;
 					
 				case "H" :
-					if (1) {
+					if ($j < 2 || ($i > 2 && $i < 5) || $j >= 6) {
 						$colorToDisplay = $color;
 						$letterToDisplay = $letter;
 					}
@@ -134,9 +134,14 @@ function drawLetter($letter, $color) {
 					break;
 
 				case "K" :
-					if (1) {
+					if ($j < 2 || ($j == (7 - $i)) || ($j == $i) || (($j + 1) == $i) || (($j + 1) == (7 - $i))) {
 						$colorToDisplay = $color;
 						$letterToDisplay = $letter;
+						
+						if(($i == 2 && $j == 2) || ($i == 5 && $j == 2)){
+							$colorToDisplay = "white";
+		   					 $letterToDisplay = "";
+						}
 					}
 					break;
 					
@@ -211,7 +216,7 @@ function drawLetter($letter, $color) {
 					break;
 					
 				case "X" :
-					if (1) {
+					if ($i == $j || ($i+$j) == 7){
 						$colorToDisplay = $color;
 						$letterToDisplay = $letter;
 					}
@@ -301,10 +306,10 @@ function drawDigit($digit, $color) {
 					break;
 					
 				case "5" :
-					if (1) {
+					if ($i < 2 || $i > 5 || $i==3 || $i ==4 || ($j<2 && $i<4)  || ($j>5 && $i>4)) {
 						$colorToDisplay = $color;
 						$digitToDisplay = $digit;
-					}
+					}		
 					break;
 					
 				case "6" :
@@ -315,7 +320,9 @@ function drawDigit($digit, $color) {
 					break;
 
 				case "7" :
-					if (1) {
+					if ($i == 0 || ($i == 1 && ($j == 6 || $j == 7)) || ($i == 2 && ($j == 5 || $j == 6)) ||
+					($i == 3 && ($j == 4 || $j == 5)) || ($i == 4 && ($j == 3 || $j == 4)) || ($i == 5 && ($j == 2 || $j == 3)) ||
+					($i == 6 && ($j == 1 || $j == 2)) || ($i == 7 && ($j == 0 || $j == 1))) {
 						$colorToDisplay = $color;
 						$digitToDisplay = $digit;
 					}					
@@ -336,6 +343,10 @@ function drawDigit($digit, $color) {
 					break;
 					
 			}  //endSwitch
+			
+			if ($color == "rainbow"){
+				$color = "rgb(" .rand(0,255) . "," .rand(0,255) . ",".rand(0,255) . ")";
+			}
 			
 			echo "<td style = 'background-color:$colorToDisplay'>";
 			echo $digitToDisplay;
