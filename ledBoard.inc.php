a30c0eb441929fb7cb4da327b8b7024e77380f80
HyunSang Yoo <yoo9408@mlc104.csumb.edu>
2015-09-10 11:24:56 -0700

    number 7

--- ledBoard.inc.php
+++ ledBoard.inc.php
@@ -310,7 +310,9 @@ function drawDigit($digit, $color) {
 					break;
 
 				case "7" :
-					if (1) {
+					if ($i == 0 || ($i == 1 && ($j == 6 || $j == 7)) || ($i == 2 && ($j == 5 || $j == 6)) ||
+					($i == 3 && ($j == 4 || $j == 5)) || ($i == 4 && ($j == 3 || $j == 4)) || ($i == 5 && ($j == 2 || $j == 3)) ||
+					($i == 6 && ($j == 1 || $j == 2)) || ($i == 7 && ($j == 0 || $j == 1))) {
 						$colorToDisplay = $color;
 						$digitToDisplay = $digit;
 					}					
